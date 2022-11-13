a17baf6769d83f0aa8558a64b304b5d8cc87958c
Ike Maldonado <maldonadoike@gmail.com>
2022-11-13 15:02:28 -0800

    add daisyui to first page

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -108,16 +108,20 @@ export class Layout extends Component {
             </div>
           ) : (
             <div>
-              <form onSubmit={this.handleSubmit}>
-                <div>
-                  <label>Name</label>
+              <form className="space-y-2" onSubmit={this.handleSubmit}>
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
+                    <span className="label-text">What is your name?</span>
+                  </label>
                   <input
                     type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
                     value={this.state.name}
                     onChange={this.handleNameChange}
-                  ></input>
+                  />
                 </div>
-                <button type="submit" disabled={this.state.name < 1}>
+                <button className="btn btn-primary w-64" type="submit" disabled={this.state.name < 1}>
                   Submit
                 </button>
               </form>
